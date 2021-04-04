7959aa1a35666c97a1698fb6666ddec070126554
Irene Font Peradejordi <if76@cornell.edu>
2021-04-04 11:52:50 -0400

    gestures

--- Lab 4/snake.py
+++ Lab 4/snake.py
@@ -4,6 +4,7 @@ import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
+from adafruit_apds9960.apds9960 import APDS9960
 from time import strftime, sleep
 import random
 
@@ -14,9 +15,9 @@ import adafruit_mpr121
 i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 ############### For gesture sensors
-apds9960 = adafruit_apds9960.apds9960.APDS9960(i2c)
-apds9960.enable_proximity = True
-apds9960.enable_gesture = True
+apds = APDS9960(i2c)
+apds.enable_proximity = True
+apds.enable_gesture = True
 
 ############### For screen display
 
